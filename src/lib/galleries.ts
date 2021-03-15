daa21e19acc25e5e871155127c084f9ad6a4f1c7
romyabbrederis <romy.abbrederis@gmail.com>
2021-03-15 09:15:55 +0100

    added programme

---
+++ src/lib/galleries.ts
@@ -0,0 +1,22 @@
+import galleries from "../../meta/galleries.yml"
+
+export type GalleryContent = {
+  readonly slug: string;
+  readonly name: string;
+  readonly address: string;
+  readonly phone: string;
+  readonly location: object;
+  readonly email: string;
+}
+
+const galleryMap: { [key: string]: AuthorContent } = generateGalleryMap();
+
+function generateGalleryMap(): { [key: string]: GalleryContent } {
+  console.log('galleries', galleries)
+  return result;
+}
+
+function getGalleryContent(slug: string) {
+  console.log('galleries', slug)
+  return  galleryMap[slug]
+} 
