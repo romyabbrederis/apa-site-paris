cc5d01e74bf3d178d4196cc18805ffea65961f91
romyabbrederis <romy.abbrederis@gmail.com>
2021-03-23 11:41:21 +0100

    copied entries into [path] english and french version

--- src/pages/fr/actualites/[path].tsx
+++ src/pages/fr/actualites/[path].tsx
@@ -1,12 +1,17 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { GetStaticPaths } from "next";
 import Head from "next/head";
 import BasicMeta from "../../../components/meta/BasicMeta";
 import OpenGraphMeta from "../../../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../../../components/meta/TwitterCardMeta";
+import Article from "../../../components/Article";
 import config from "../../../lib/config";
-import { ArticleContent, fetchArticlesContent, fetchArticleContent } from "../../../lib/articles";
-import { useEffect } from 'react';
+import {
+  ArticleContent,
+  fetchArticlesContent,
+  fetchArticleContent,
+} from "../../../lib/articles";
+import { useEffect } from "react";
 
 type Props = {
   article: ArticleContent;
@@ -15,39 +20,38 @@ type Props = {
 };
 
 export default function Index({ article, language, params }: Props) {
-  const router = useRouter()
-  const { path } = router.query
-  const url = "/fr/actualites" + path;
-  const title = "Actualités";
+  const router = useRouter();
+  const { path } = router.query;
+  const url = "/fr/actualites/" + path;
+  const title = article.title;
 
-
-  console.log("article", article, language, params)
+  console.log("article", article, language, params, title);
   return (
     <div>
       <BasicMeta url={url} title={title} />
       <OpenGraphMeta url={url} title={title} />
       <TwitterCardMeta url={url} title={title} />
-      <h1>{title}</h1>
+      {article ? <Article article={article} /> : null}
     </div>
   );
 }
 
 export async function getStaticPaths() {
-  const articles = fetchArticlesContent('fr')
+  const articles = fetchArticlesContent("fr");
   const paths = articles.map((item) => ({
     params: { path: item.slug },
-  }))
-  return { paths, fallback: false }
+  }));
+  return { paths, fallback: false };
 }
 
-export const getStaticProps = async ({params, locale}) => {
-  const article = fetchArticleContent(params.path, "fr")
+export const getStaticProps = async ({ params, locale }) => {
+  const article = fetchArticleContent(params.path, "fr");
   const language = locale || null;
   return {
     props: {
       article,
       language,
-      params
+      params,
     },
   };
 };
